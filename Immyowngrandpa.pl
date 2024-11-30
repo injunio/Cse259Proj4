8b6dd90a751fa2497c6a691f4e3f20a7ebbbe324
diyaS15 <dshriva6@asu.edu>
2024-11-29 22:59:57 -0700

    changes to the file
    - changed the file name since there was an error in running consult
    - changed me to i (in facts) to be consistent with test cases
    - moved some rules from silly extra section to main rules (to match test cases)

--- Immyowngrandpa.pl
+++ Immyowngrandpa.pl
@@ -7,7 +7,7 @@
 % -----------------------------------------------------------------------%
 
 % who are male/female
-male(me).
+male(i).
 male(my_father).
 male(bouncing_baby_boy).
 male(ne_the_run_kid).
@@ -16,15 +16,15 @@ female(red_hair_grown_daughter_of_widow).
 
 % who are married
 % the fact is a biconditional
-married(me,pretty_widow).
-married(pretty_widow,me).
+married(i,pretty_widow).
+married(pretty_widow,i).
 married(my_father,red_hair_grown_daughter_of_widow).
 married(red_hair_grown_daughter_of_widow,my_father).
 
 % child_of(kid, parent1, parent 2)
 % there are two parents to make this easier to figure out who are the bio parents and step-parents
-child_of(me, my_father, unknown).
-child_of(bouncing_baby_boy, pretty_widow, me).
+child_of(i, my_father, unknown).
+child_of(bouncing_baby_boy, pretty_widow, i).
 child_of(red_hair_grown_daughter_of_widow, unknown, pretty_widow).
 child_of(on_the_run_kid, my_father, red_hair_grown_daughter_of_widow).
 
@@ -48,6 +48,7 @@ wife(X,Y) :- female(X), married(Y,X).
 parent(X,Y) :-
     child_of(Y,X,_);
     step_parent(X,Y).
+    % should we add biological_parent here? 
 
 % parent_in_law(X,Z) X is parent of persons spouse, Z is person in question
 parent_in_law(X,Z) :-
@@ -70,9 +71,20 @@ grandparent(X,Y) :-
     parent(Z,Y),
     parent(X,Z).
 
-% sibling(X,Y)
-% sibling_in_law(X,Y)
-% uncle_aunt(X,Z)
+% sibling(X,Y)  X is the sibling of Y, meaning they have at least one shared parent 
+% sibling_in_law(X,Y) X is the sibling in law of Y, meaning X is married to the sibling of Y or Y is married to the sibling of X
+% uncle_aunt(X,Z) X is uncle/aunt and Z is niece/nephew, meaning X is sibling of the parent of Z, or Z is the child_of the sibling of X
+
+
+% NOTE: we might have to add these since they were in the test case on the slide, 
+%       I just moved them from the last section up here but we can move it back if needed
+
+% mother(X,Y) :- parent(X,Y), female(X)
+% grandchild(X,Y) 
+% grandmother(X,Y) :- grandparent(X,Y), female(X).
+% grandfather(X,Y) :- grandparent(X,Y), male(X).
+% brother(X,Y) :- male(X).
+% daughter(X,Y) :- child (X,Y), female(Y).
 
 % -----------------------------------------------------------------------%
 % These are just silly extra stuff if we wanna go overboard.             %
@@ -80,10 +92,6 @@ grandparent(X,Y) :-
 %   the rules above.                                                     %
 % -----------------------------------------------------------------------%
 
-% daughter(X,Y)
-% son(X,Y)
-% brother(X,Y)
-% mother(X,Y)
-% grandchild(X,Y)
-% grandmother(X,Y)
-% grandfather(X,Y)
+% daughter(X,Y) :- child (X,Y), female(Y)
+% son(X,Y) :- child (X,Y), male(Y)
+
